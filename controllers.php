0c2d1ee7328ccddf01d0a5076baaa1d28928cb50
felix <felixp4@yandex.ru>
2017-05-09 12:13:57 +0300

    Иправления в Controller.php

--- controllers.php
+++ controllers.php
@@ -3,12 +3,12 @@
 
 function list_action()
 {
-    $posts = get_all_posts($_GET['id']);
+    $posts = get_all_posts();
     require 'templates/list.php';
 }
 
 function show_action($id)
 {
-    $post = get_post_by_id($_GET['id']);
+    $post = get_post_by_id($id);
     require 'templates/show.php';
 }
