b01e514cad3bc5d0a27a5394dee87ee2e4efe83d
felix <felixp4@yandex.ru>
2017-05-09 09:18:30 +0300

    Isolating th Presentation

--- index.php
+++ index.php
@@ -3,30 +3,13 @@
 $link = new PDO ("mysql:host=localhost; dbname=CRUD", 'root', 'phpschool17');
 
 $result = $link->query("SELECT id, name FROM article");
-?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>List of posts</title>
-    </head>
-    <body>
-        <h1>List of posts</h1>
-        <ul>
-            <?php while($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-            <li>
-                <a href="/show.php?id=<?= $row['id']?>">
-                    <?= $row['name'] ?>
-                </a>
-            </li>
-            <?php endwhile?>
-        </ul>
-    </body>
-</html>
+$posts = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $posts[] = $row;
+}
 
-<?php
-    $link = null;
-?>
+$link = null;
 
-
-
+// include the HTML presentation code
+require 'templates/list.php';
