0fa9b4d75abcc114136486a977fb4a36a9ca4f43
felix <felixp4@yandex.ru>
2017-05-09 09:58:19 +0300

    Isolating the Layout

--- templates/list.php
+++ templates/list.php
@@ -1,20 +1,18 @@
 <!-- templates/list.php -->
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>List of posts</title>
-</head>
-<body>
+<?php $title='List of posts' ?>
+
+<?php ob_start() ?>
 <h1>List of posts</h1>
-<ul>
-    <?php foreach($posts as $post): ?>
-        <li>
-            <a href="/show.php?id=<?= $post['id'] ?>">
-                <?= $post['name'] ?>
-            </a>
-        </li>
-    <?php endforeach ?>
-</ul>
-</body>
-</html>
+    <ul>
+        <?php foreach($posts as $post): ?>
+            <li>
+                <a href="/show.php?id=<?= $post['id'] ?>">
+                    <?= $post['name'] ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
