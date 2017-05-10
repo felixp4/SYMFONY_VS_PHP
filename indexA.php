0c30bad19d6917f489a87da0c895437bd7277822
felix <felixp4@yandex.ru>
2017-05-10 10:23:52 +0300

    Add a Touch of Symfony

---
+++ indexA.php
@@ -0,0 +1,20 @@
+<?php
+// index.php
+
+// load an initialize any global libraries
+require_once 'model/model.php';
+require_once 'controllers.php';
+
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ('/index.php' === $uri) {
+    list_action();
+}
+elseif ('/index.php/show' === $uri && isset($_GET['id'])){
+    show_action($_GET['id']);
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
+
